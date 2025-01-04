5b8a4005cdb6ab36a9ffa926749c057b158f8693
Akash Chand <akashchand1703@gmail.com>
2025-01-05 01:51:50 +0530

    fix base url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,8 +2,10 @@ export function getBaseUrl() {
     if (typeof window !== 'undefined') {
         return ''; // Browser should use relative path
     }
-    if (process.env.VERCEL_URL) {
-        return `https://${process.env.VERCEL_URL}`;
+    console.log({NODE_ENV: process.env.NODE_ENV});
+    if (process.env.NODE_ENV != "development") {
+        console.log("here");
+        return `https://doosra-frontend.vercel.app/`; // TODO: FIX THIS
     }
     if (process.env.RENDER_INTERNAL_HOSTNAME) {
         return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
